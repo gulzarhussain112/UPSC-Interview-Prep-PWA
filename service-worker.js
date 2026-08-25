3ef247812ebe479c24873476fc57e33ee3a3f415
Gulzar Hussain <59336131+gulzarhussain112@users.noreply.github.com>
2026-08-25 15:53:23 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,86 @@
-const CACHE='upsc-prep-v13.2.1';const ASSETS=[
- "./",
- "./index.html",
- "./style.css",
- "./data.js",
- "./app.js","./firebase-config.js","./firebase-client.js","./firebase-messaging-sw.js",
- "./manifest.json",
- "./assets/icons/icon-192.png",
- "./assets/icons/icon-512.png",
- "./assets/screenshots/desktop-wide.png",
- "./assets/screenshots/mobile.png"
-];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
+const CACHE = 'upsc-prep-v14';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './data.js',
+  './app.js',
+  './firebase-config.js',
+  './firebase-client.js',
+  './firebase-messaging-sw.js',
+  './manifest.json',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/screenshots/desktop-wide.png',
+  './assets/screenshots/mobile.png'
+];
+
+const NETWORK_FIRST = [
+  './',
+  './index.html',
+  './style.css',
+  './data.js',
+  './app.js',
+  './firebase-config.js',
+  './firebase-client.js',
+  './firebase-messaging-sw.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  const isNetworkFirst = NETWORK_FIRST.some(path =>
+    url.pathname.endsWith(path.replace('./', ''))
+  );
+
+  if (isNetworkFirst) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
